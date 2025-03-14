abeb9a7ee24be21b70e359e9364c033b9433b418
Henry <xero0001@naver.com>
2025-03-14 22:47:39 +0900

    Add screen breakpoint

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,30 +1,18 @@
-import { Geist, Geist_Mono } from "next/font/google"
-
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
-
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+import "@workspace/ui/globals.css";
+import { Providers } from "@/components/providers";
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+        className={`max-w-screen-mobile mx-auto select-none scroll-smooth font-sans antialiased shadow-sm`}
       >
         <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
