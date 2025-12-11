5e5fb1b6a86a5bd446e3b979f41879ee2184582e
boozeal <skyladder8895@gmail.com>
2025-12-11 10:12:10 +0900

    fix: remove middleware

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,7 +2,9 @@ import "@repo/ui/globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "@repo/ui/components/sonner";
 import { Metadata } from "next";
+
 import { METADATA } from "@/shared/consts/metadata";
+
 
 export const metadata: Metadata = {
   title: `${METADATA.TITLE}`,
@@ -23,7 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`mx-auto scroll-smooth font-sans antialiased shadow-sm`}>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+        </Providers>
         <Toaster />
       </body>
     </html>
