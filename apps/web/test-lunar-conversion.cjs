88f67fec855ab58a099b3265e49fc21561e0fef2
henry <xero0001@naver.com>
2025-12-18 01:08:42 +0900

    fix

--- apps/web/test-lunar-conversion.cjs
+++
@@ -1,8 +0,0 @@
-const { Lunar } = require("lunar-javascript");
-
-const lunar = Lunar.fromYmdHms(1981, 1, 21, 12, 0, 0, false);
-console.log(`Input Lunar: 1981-01-21 12:00:00`);
-console.log(`Converted Solar: ${lunar.getSolar().toFullString()}`);
-console.log(`Solar Year: ${lunar.getSolar().getYear()}`);
-console.log(`Solar Month: ${lunar.getSolar().getMonth()}`);
-console.log(`Solar Day: ${lunar.getSolar().getDay()}`);
