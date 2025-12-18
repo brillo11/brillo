f08fa5ce0f2678aa88823dd81edf4feb50d71489
henry <xero0001@naver.com>
2025-12-18 19:23:23 +0900

    fix

--- apps/web/shared/consts/metadata.ts
+++ apps/web/shared/consts/metadata.ts
@@ -1,7 +1,6 @@
 export const METADATA = {
-  TITLE: "TubeInsight - YouTube 컨텐츠 제작 어시스턴트 플랫폼",
-  DESCRIPTION:
-    "AI 어시스턴트가 페르소나부터 대본, 썸네일, 메타데이터까지 YouTube 콘텐츠 제작의 모든 과정을 도와드립니다",
+  TITLE: "VizionX",
+  DESCRIPTION: "",
   THUMBNAIL: "/thumbnail.png",
   COMPANY_NAME: "TALOS WORKS",
   // 절대 URL 버전 추가
