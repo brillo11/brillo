d993970d91bf041b73d35e1eae51fdd8952d6a15
boozeal <skyladder8895@gmail.com>
2026-02-10 16:43:02 +0900

    feat: Add sidebar

--- apps/web/features/layout/components/Header.tsx
+++ apps/web/features/layout/components/Header.tsx
@@ -1,13 +1,23 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { LoginButton } from "@/features/auth/LoginButton";
 import { cn } from "@/shared/lib/utils";
+import { MobileSidebar } from "./MobileSidebar";
 
 export function Header() {
   const pathname = usePathname();
   const isDarkHeader = pathname === "/" || pathname === "/about";
+
+  const navItems = [
+    { label: "About", href: "/about" },
+    { label: "Woman", href: "/woman" },
+    { label: "Man", href: "/man" },
+    { label: "VIP/Celeb", href: "/vip" },
+    { label: "FAQ", href: "/faq" },
+  ];
 
   return (
     <header
@@ -16,28 +26,27 @@ export function Header() {
         isDarkHeader ? "bg-black" : "bg-white border-b border-gray-100/50",
       )}
     >
-      <div className="relative w-full mx-auto top-0 h-full px-4 xl:px-[18px] flex items-center">
-        {/* Logo */}
-        <Link href="/">
-          <img
-            className={cn(
-              "w-[93px] h-[35px] object-contain transition-all duration-300",
-              !isDarkHeader && "invert",
-            )}
-            alt="Brillo Logo"
-            src="/images/layout/brillo-logo-text.png"
-          />
-        </Link>
+      <div className="relative w-full mx-auto top-0 h-full px-4 xl:px-[18px] flex items-center justify-between md:justify-normal">
+        {/* Mobile Hamburger - Left */}
+        <MobileSidebar isDark={isDarkHeader} navItems={navItems} />
 
-        {/* Navigation */}
-        <div className="hidden md:inline-flex items-center gap-[33px] absolute left-1/2 -translate-x-1/2">
-          {[
-            { label: "About", href: "/about" },
-            { label: "Woman", href: "/woman" },
-            { label: "Man", href: "/man" },
-            { label: "VIP/Celeb", href: "/vip" },
-            { label: "FAQ", href: "/faq" },
-          ].map((item) => (
+        {/* Logo - Center on Mobile, Left on Desktop */}
+        <div className="absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0 md:mr-auto">
+          <Link href="/">
+            <img
+              className={cn(
+                "w-[93px] h-[35px] object-contain transition-all duration-300",
+                !isDarkHeader && "invert",
+              )}
+              alt="Brillo Logo"
+              src="/images/layout/brillo-logo-text.png"
+            />
+          </Link>
+        </div>
+
+        {/* Desktop Navigation - Center */}
+        <div className="hidden md:absolute md:left-1/2 md:-translate-x-1/2 md:flex items-center gap-[33px]">
+          {navItems.map((item) => (
             <Link
               key={item.label}
               href={item.href}
@@ -51,10 +60,10 @@ export function Header() {
           ))}
         </div>
 
-        {/* User Icon / Login */}
+        {/* User Icon / Login - Right */}
         <div
           className={cn(
-            "absolute right-4 lg:right-[18px] transition-all duration-300",
+            "relative transition-all duration-300",
             !isDarkHeader && "invert opacity-80 hover:opacity-100",
           )}
         >
