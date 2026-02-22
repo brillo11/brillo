026cd7cbfe0eceee2b45d45c1ab73d410658664a
henry <xero0001@naver.com>
2026-02-23 01:43:02 +0900

    1

--- apps/web/shared/consts/metadata.ts
+++ apps/web/shared/consts/metadata.ts
@@ -1,6 +1,6 @@
 export const METADATA = {
-  TITLE: "brillo",
-  DESCRIPTION: "",
+  TITLE: "브릴로",
+  DESCRIPTION: "당신만의 프리미엄 퍼스널 비주얼디렉팅",
   THUMBNAIL: "/thumbnail1.png",
   COMPANY_NAME: "BRILLO",
   // 절대 URL 버전 추가
